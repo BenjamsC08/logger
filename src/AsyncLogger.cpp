2fab2a3a72fe4ad95033597edfbeeee0d7a794c6
moa <sirus_m@123456789>
2026-05-15 07:07:03 +0200

    the idea

--- src/AsyncLogger.cpp
+++ src/AsyncLogger.cpp
@@ -1,2 +1,48 @@
 #include "AsyncLogger.hpp"
+#include "def.hpp"
+#include <mutex>
 
+AsyncLogger::AsyncLogger(ILogger *logger) : endRequested_(false)
+{
+	this->loggers_.push_back(logger);
+}
+
+AsyncLogger::~AsyncLogger()
+{
+	std::lock_guard<std::mutex> lock(this->mtxMsg_);
+	this->endRequested_ = true;
+	this->tLog_.join();
+	for (t_uint i=0; i < this->loggers_.size(); i++)
+	{
+		delete this->loggers_[i];
+	}
+}
+
+/*			Interface		  */
+void	AsyncLogger::Log(const std::string &message, int level)
+{
+	std::lock_guard<std::mutex> lock(this->mtxMsg_);
+	this->msgs_.push_back(message);
+	this->levels_.push_back(level);
+	this->cv_.notify_all();
+}
+
+void	AsyncLogger::SetMinLevel(int level)
+{	(void)level;	}
+
+void	AsyncLogger::AddLogger(ILogger *logger)
+{
+	//mutex 2
+	std::lock_guard<std::mutex> lock(this->mtxLog_);
+	this->loggers_.push_back(logger);
+	//unmutex 2
+}
+
+void	AsyncLogger::Run()
+{
+	std::unique_lock<std::mutex> lockLog(this->mtxLog_);
+	std::unique_lock<std::mutex> lock(this->mtxMsg_);
+	while (this->msgs_.empty() && !this->endRequested_)
+		this->cv_.wait(lock);
+
+}
--- src/AsyncLogger.hpp
+++ src/AsyncLogger.hpp
@@ -1,5 +1,42 @@
 #ifndef ASYNCLOGGER_HPP
 # define ASYNCLOGGER_HPP
 
+#include "ILogger.hpp"
+#include <condition_variable>
+#include <deque>
+#include <mutex>
+#include <string>
+#include <thread>
+#include <vector>
+class AsyncLogger : public ILogger
+{
+public:
+	AsyncLogger() = delete;
+	explicit AsyncLogger(ILogger *);
+	AsyncLogger(const AsyncLogger &) = delete;
+	virtual ~AsyncLogger();
+
+	AsyncLogger &operator=(const AsyncLogger &) = delete;
+	/*			Interface		  */
+	virtual void	Log(const std::string &message, int level);
+
+	void	AddLogger(ILogger *);
+	void	AddMessage(const std::string &);
+
+
+private:
+	void	Run();
+	virtual void	SetMinLevel(int level);
+	std::vector<ILogger *>	loggers_;
+	std::deque<std::string>	msgs_;
+	std::deque<int>			levels_;
+	std::thread				tLog_;
+	std::mutex				mtxMsg_;
+	std::mutex				mtxLog_;
+	std::condition_variable	cv_;
+	bool					endRequested_;
+
+
+};
 
 #endif
